3f3c6a0f87f9467590bab8619a9ae7ed5c74b94e
Christine Stoner <acct252000@gmail.com>
2016-11-20 21:10:55 -0600

    Update comments on database definition

--- tournament.sql
+++ tournament.sql
@@ -3,13 +3,13 @@
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
--- table to hold player information, including name, timestamp, id, score, matches_played, and win_percentage
+-- table to hold player information, including name, timestamp, id
 CREATE TABLE players ( name TEXT,
                        time_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        id SERIAL primary key
                        );
 
--- table to hold match information, including match id, round, first and second player id, and winning player id
+-- table to hold match information, including winning and losing player id
 CREATE TABLE matches ( match_id SERIAL primary key,
 			                 winning_player_id INTEGER references players (id),
                        losing_player_id INTEGER references players (id)
